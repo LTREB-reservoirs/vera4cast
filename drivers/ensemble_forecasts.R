9900b0a2a2021c981ff0cccd601446ca30612281
Austin Delany <addelany@gmail.com>
2026-09-21 16:39:10 -0500

    Comment out healthcheck URL check
    Comment out healthcheck URL existence check

--- drivers/ensemble_forecasts.R
+++ drivers/ensemble_forecasts.R
@@ -43,4 +43,4 @@ download_ensemble_forecast(model_id, forecast_horizon = 10, sites = "fcre")
 submit_met_forecast(model_id)
 
 ## Call healthcheck
-RCurl::url.exists("https://hc-ping.com/09ad92fc-a41c-496d-96c2-94974dc2dfdb", timeout = 5)
+#RCurl::url.exists("https://hc-ping.com/09ad92fc-a41c-496d-96c2-94974dc2dfdb", timeout = 5)
